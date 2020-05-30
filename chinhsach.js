d10b682db1c4b80df272151912ae75733b31034d
PhucBinh <phucbinh.2001@gmail.com>
2020-05-30 12:29:27 +0700

    dfgdfg

--- chinhsach.js
+++ chinhsach.js
@@ -50,3 +50,22 @@ function offnav() {
     navbarrg.style.display = "none";
 }
 // navbar
+
+var boxgiasu = document.getElementById("giasu");
+var boxhocvien = document.getElementById("hocvien");
+var btngiasu = document.getElementById("btngiasu");
+var btnhocvien = document.getElementById("btnhocvien");
+
+function giasu() {
+    boxhocvien.style.display = "none";
+    boxgiasu.style.display = "block";
+    btnhocvien.classList.remove("active");
+    btngiasu.classList.add("active");
+}
+
+function hocvien() {
+    boxgiasu.style.display = "none";
+    boxhocvien.style.display = "block";
+    btnhocvien.classList.add("active");
+    btngiasu.classList.remove("active");
+}
